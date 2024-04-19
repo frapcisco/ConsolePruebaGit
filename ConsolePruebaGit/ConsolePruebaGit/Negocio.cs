b7c7c8babb65c51cd6ba31378e9702b30eb68970
Francisco Moraga Serey <fmoragas@sence.cl>
2024-04-19 11:29:48 -0400

    primera subida

---
+++ ConsolePruebaGit/ConsolePruebaGit/Negocio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePruebaGit
+{
+    public class Negocio
+    {
+        public DateTime ObtenerFecha()
+        {
+            return DateTime.Now;
+        }
+
+
+    }
+}
---
+++ ConsolePruebaGit/ConsolePruebaGit/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsolePruebaGit;
+
+Console.WriteLine("Hello, World!");
+
+Negocio negocio= new Negocio();
+
+Console.WriteLine(negocio.ObtenerFecha().Date.Year.ToString());
